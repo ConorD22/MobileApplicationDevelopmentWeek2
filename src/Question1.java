22edcf4d908e73e18f473ad816207dcaa6f60902
Conor <duffy-c76@ulster.ac.uk>
2023-10-04 16:40:36 +0100

    Question1

--- src/Question1.java
+++ src/Question1.java
@@ -10,8 +10,6 @@ public class Question1 {
         for(int i = 1; i <= num; i++ ){
             if(i % 2 == 0){
                 sum += i;
-            }else{
-
             }
         }
         System.out.print("Sum: " + sum);
