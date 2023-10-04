5ca852e9509800b8981a960f6146827bac9f250f
Conor <duffy-c76@ulster.ac.uk>
2023-10-04 16:40:12 +0100

    Question1

---
+++ src/Question1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int num = scan.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i <= num; i++ ){
+            if(i % 2 == 0){
+                sum += i;
+            }else{
+
+            }
+        }
+        System.out.print("Sum: " + sum);
+    }
+}
