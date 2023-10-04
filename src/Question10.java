c260f45ef82bf1299198f3297ad832108634271d
Conor <duffy-c76@ulster.ac.uk>
2023-10-04 21:00:28 +0100

    Question6

---
+++ src/Question10.java
@@ -0,0 +1,38 @@
+public class Question10 {
+    public static void main(String[] args) {
+        String enter = "Peter";
+        check(enter);
+    }
+
+    public static void check(String a){
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        String lowerCase = a.toLowerCase();
+        boolean result = true;
+        char first = lowerCase.charAt(0);
+
+
+
+        for(int i = 0; i < vowels.length; i++){
+            if(lowerCase.charAt(0) == vowels[i]){
+                result = true;
+                break;
+            }else{
+                result = false;
+            }
+        }
+
+        if(result){
+            System.out.println(a + "-ay");
+        }else{
+            String removed = a.replace(a.charAt(0), ' ');
+            System.out.println(removed + "-" + lowerCase.charAt(0) + "ay");
+        }
+
+
+
+
+
+
+
+    }
+}
--- src/Question6.java
+++ src/Question6.java
@@ -1,40 +1,5 @@
 import java.util.Arrays;
 
 public class Question6 {
-    public static void main(String[] args) {
-        String enter = "Adam";
-        check(enter);
-    }
 
-    public static void check(String a){
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        String lowerCase = a.toLowerCase();
-        boolean result = true;
-        char first = lowerCase.charAt(0);
-
-
-
-        for(int i = 0; i < vowels.length; i++){
-            if(lowerCase.charAt(0) == vowels[i]){
-                result = true;
-                break;
-            }else{
-                result = false;
-            }
-        }
-
-            if(result){
-                System.out.println(a + "-ay");
-            }else{
-                String removed = a.replace(a.charAt(0), ' ');
-                System.out.println(removed + "-" + lowerCase.charAt(0) + "ay");
-            }
-
-
-
-
-
-
-
-    }
 }
